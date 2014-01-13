f1f9c29355b03dd804191dba2d4af8b90a3f45cd
Oleksiy Krivoshey <oleksiy@luckyteam.co.uk>
2014-01-13 21:28:09 +0200

    More close test

--- perf/test.js
+++ perf/test.js
@@ -7,14 +7,33 @@ var testfile = path.dirname(__filename) + '/image.png';
 
 var buf = fs.readFileSync(testfile);
 
+function varintByteLength(number, signed) {
+    var n = 0;
+
+    if (signed) {
+        number = (number << 1) ^ (number >> 31);
+    }
+
+    while (number !== 0) {
+        number = number >>> 7;
+        n ++;
+    }
+
+    return n;
+}
+
 function testWithArray() {
     var result = [];
-    result = result.concat(buf);
+    var value = Array.prototype.slice.call(buf);
+    result = result.concat(value);
     result = new Buffer(result);
     return result;
 }
 
 function testWithBuffer() {
+    for (var i = 0; i < 50; i++) {
+        varintByteLength(i * 1024);
+    }
     var result = new Buffer(buf.length);
     buf.copy(result);
     return result;
